70c0ef3f69b711e3a06a988a587be18eca00e841
simonarellano <simon.arellano@autosentinel.mx>
2026-04-21 10:38:32 -0600

    Se revisa app

--- aeromx/frontend/vite.config.js
+++ aeromx/frontend/vite.config.js
@@ -25,11 +25,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true
       }
     }
